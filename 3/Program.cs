e3e637bdab59d36da5942cb0366789a358060fd2
MOKProgramist <is24romanovar@artcollege.ru>
2021-10-29 12:09:46 +0300

    Добавьте файлы проекта.

---
+++ 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i; // объявляем переменные
+            i = 0; // задаем переменной значение
+
+            Console.Write("Число: "); // вывод на экран
+            num = Convert.ToInt32(Console.ReadLine()); // запись числа введенного пользователем
+
+            while (num > 0) // Проверка перемеменной
+            {
+                i++; // Переменная i инкрементируется
+                num = num / 10; // Деление переменной num на 10
+                Console.WriteLine(num); // выводи суимму после действия
+            }
+
+            Console.WriteLine("Кол-во периодов: " + i); // вывод на экран
+            Console.ReadKey(); // задерживаем консоль
+
+        }
+    }
+}
---
+++ While_29.10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace While_29._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+                byte x = 0; // переменная для цикла
+                double y; // переменная для функции
+
+                while (x <= 6) // пока значение х меньше или равно конечному
+                {
+                    y = Math.Pow(x, 2) - 8; //нахождение функции
+                    Console.WriteLine($"y = {Math.Round(y,1)} x = {x}"); //вывод
+                    x += 1; //шаг +1 
+
+                }
+
+                Console.ReadKey(); // задержать консоль
+        }
+    }
+}
